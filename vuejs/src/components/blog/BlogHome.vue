3508c4252ce9b0e249514eaa4ebd8baf4e5f3384
funkyfisch <funkyfisch@gmail.com>
2018-04-02 02:41:13 +0200

    Ported files to new subURL : home

---
+++ vuejs/src/components/blog/BlogHome.vue
@@ -0,0 +1,120 @@
+<template>
+  <div>
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="index.html">John Gkikas - My Blog </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li>
+                        <a href="about.html">About</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Header -->
+    <!-- Set your background image for this header on the line below. -->
+    <header class="intro-header" style="background-image: url('img/bannerindex.jpg')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="site-heading">
+                        <h1 id="heading">My personal tech blog</h1>
+                        <hr class="small">
+                        <span id="heading"class="subheading">for fellow tinkerers and programmers</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <div class="post-preview">
+                    <a href="post.html">
+                        <h2 class="post-title">
+                            Remote code deployment with SSH and Platform.io
+                        </h2>
+                        <h3 class="post-subtitle">
+                            On a Raspberry Pi + microcontrollers
+                        </h3>
+                    </a>
+                    <p class="post-meta">Initially posted on August 14, 2017</p>
+                </div>
+                <hr>
+                <!-- Pager -->
+                <ul class="pager">
+                    <li class="next">
+                        <a href="#">Older Posts &rarr;</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+
+                    <ul class="list-inline text-center">
+                        <li>
+                            <a href="https://www.linkedin.com/in/ioannis-gkikas-0b5a6010b/">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.github.com/funkyfisch">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                    <p class="copyright text-muted">Copyleft &copy; me myself and I 2k17</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BlogHome',
+  data () {
+
+  }
+}
+</script>
+
+<style>
+</style>
--- vuejs/src/router/index.js
+++ vuejs/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/PlanetOrbits'
+import BlogHome from '@/components/blog/BlogHome'
+import HelloWorld from '@/components/home/PlanetOrbits'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'PlanetOrbits',
       component: HelloWorld
+    },
+    {
+      path: '/blog',
+      name: 'Blog',
+      component: BlogHome
     }
   ]
 })
